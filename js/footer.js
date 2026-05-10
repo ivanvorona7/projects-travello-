8956e1b044a3a108ea4fe8affd1d04176ef22817
Ivan Vorona <ivorona07@gmail.com>
2026-05-10 23:33:56 +0300

    Eror

--- js/footer.js
+++ js/footer.js
@@ -2,19 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     function setupModal(btnId, modalId) {
         const btn = document.getElementById(btnId);
         const modal = document.getElementById(modalId);
-        const closeBtn = modal.querySelector('[data-close]');
 
         if (btn && modal) {
+            const closeBtn = modal.querySelector('[data-close]');
+
             btn.addEventListener('click', (e) => {
                 e.preventDefault();
                 modal.classList.add('is-open');
                 document.body.classList.add('no-scroll');
             });
 
-            closeBtn.addEventListener('click', () => {
-                modal.classList.remove('is-open');
-                document.body.classList.remove('no-scroll');
-            });
+            if (closeBtn) {
+                closeBtn.addEventListener('click', () => {
+                    modal.classList.remove('is-open');
+                    document.body.classList.remove('no-scroll');
+                });
+            }
 
             modal.addEventListener('click', (e) => {
                 if (e.target === modal) {
@@ -22,6 +25,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     document.body.classList.remove('no-scroll');
                 }
             });
+        } else {
+            console.warn(`Modal setup failed: Elements with ID ${btnId} or ${modalId} not found.`);
         }
     }
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
-import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
-  // Настройка сервера для удобства
-  server: {
-    open: true, // Автоматически открывать браузер при запуске
-  },
-  plugins: [injectHTML()],
+  base: '/projects-travello-/', 
 });
